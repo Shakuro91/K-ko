a8d45d2bf15ea0b3ec2fee000eaf5738db04ab78
Your Name <you@example.com>
2020-08-26 21:06:40 +0100

    cos

--- cos.py
+++ cos.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+def rysujplansze(pole1,pole2,pole3,pole4,pole5,pole6,pole7,pole8,pole9):
+    thislist = [(pole1, pole2, pole3), (pole4, pole5, pole6), (pole7, pole8, pole9)]
+    for x in thislist:
+        print(x)
+
+
+plansza = ["apple", "banana", "apple", "banana", "cherry","apple", "banana", "cherry","cherry"]
+for x in plansza:
+    username = input
+rysujplansze("apple", "banana", "apple", "banana", "cherry","apple", "banana", "cherry","cherry") 
+username = input("Enter username:")
+print("Username is: " + username)  
+ 
